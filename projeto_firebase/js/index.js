82372b891dc3ac39693135554aa991aebf922bf6
Erkmann <erkmann08@gmail.com>
2021-06-22 15:14:51 -0300

    toasting and form error

--- projeto_firebase/js/index.js
+++ projeto_firebase/js/index.js
@@ -1,3 +1,54 @@
+function formLoginExecSubmit() {
+    if ($('#email').val().length > 0 && $('#password').val().length > 0) {
+        validateFields()
+        $('#formLogin').submit();
+    }
+    else {
+        invalidateFields()
+        toastr.error('Email e senha nao podem estar vazios!')
+    }
+
+}
+
+function invalidateFields() {
+    if (!$('#email').val().length > 0) {
+        $('#emailInvalid').slideDown()
+        $('#email').addClass('is-invalid')
+    }
+    else {
+        $('#emailInvalid').slideUp()
+        $('#email').removeClass('is-invalid')
+    }
+
+    if (!$('#password').val().length > 0) {
+
+        $('#senhaInvalid').slideDown()
+        $('#password').addClass('is-invalid')
+    }
+    else {
+        $('#senhaInvalid').slideUp()
+        $('#password').removeClass('is-invalid')
+    }
+}
+
+function validateFields() {
+    $('#emailInvalid').slideUp()
+    $('#email').removeClass('is-invalid')
+    $('#senhaInvalid').slideUp()
+    $('#password').removeClass('is-invalid')
+}
+
 $(document).ready(function () {
-    
-})
+    $('#emailInvalid').hide()
+    $('#senhaInvalid').hide()
+
+    $("#formLogin").submit(function (e) {
+        e.preventDefault()
+
+        let email = $('#email').val();
+        let senha = $('#password').val();
+
+        toastr.success(email)
+        toastr.success(senha)
+    });
+});
---
+++ projeto_firebase/js/toast.js
@@ -0,0 +1,3 @@
+toastr.options.progressBar = true;
+toastr.options.timeOut = 2500;
+toastr.options.preventDuplicates = true;
